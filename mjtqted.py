49881b63c529099a5ae96d6fa044d71fb344e82f
Markus Törnqvist <mjt@fadconsulting.com>
2015-09-27 20:00:00 +0300

    New file functionality

--- mjtqted.py
+++ mjtqted.py
@@ -14,7 +14,45 @@ import sys
 class MainWindow(QtWidgets.QMainWindow):
     def __init__(self):
         QtWidgets.QMainWindow.__init__(self)
-        pass
+
+    def set_initial_state(self):
+        """Helper for things we init after ui is inited
+        """
+
+        # XXX: textEdit could be custom-inherited from QTextEdit
+        self.textEdit.saved = False
+
+    def ask_new_file(self):
+        """Split new file confirmation into its own function
+        """
+
+        msg_title = 'Confirmation'
+        msg_contents = 'Unsaved changes, are you sure you want a new file?'
+        msg_buttons = QtWidgets.QMessageBox.Yes | QtWidgets.QMessageBox.No
+        msg_default = QtWidgets.QMessageBox.No
+
+        reply = QtWidgets.QMessageBox.question(self, msg_title, msg_contents, msg_buttons, msg_default)
+
+        return reply
+
+    @QtCore.pyqtSlot(name='on_pushButton_New_clicked')
+    def new_file(self):
+        """Open a new file; if the current one's not saved, complain
+        """
+
+        editor = self.textEdit
+
+        # Toggled by listView_Outputs
+        saved = editor.saved
+
+        document = editor.document()
+        contents = document.toPlainText().strip()
+
+        if contents and not saved:
+            reply = self.ask_new_file()
+
+            if reply == QtWidgets.QMessageBox.Yes:
+                document.clear()
 
 
 if __name__ == '__main__':
@@ -23,6 +61,9 @@ if __name__ == '__main__':
     ui = uic.loadUi('mainwindow.ui', baseinstance=main_window)
 
     ui.action_Quit.triggered.connect(QtCore.QCoreApplication.instance().quit)
+
+    # Do the init we can do after ui is set up
+    main_window.set_initial_state()
 
     main_window.show()
 
